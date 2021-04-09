62d1818a0b2727d32fd49a32fa99467b32452f18
g_user <45758480+d6jpg@users.noreply.github.com>
2021-04-09 18:30:26 +0900

    section 9-5

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "hittable_list.h"
 //#include "vec3.h"
 #include "sphere.h"
+#include "material.h"
 
 #include <iostream>
 using namespace std;
@@ -13,9 +14,15 @@ color ray_color(const ray& r, const hittable& world, int depth){
   hit_record rec;
   if(depth <= 0)return color(0, 0, 0);
   if(world.hit(r, 0.001, infinity, rec)){
-    point3 target = rec.p + random_in_hemisphere(rec.normal);
+    // point3 target = rec.p + random_in_hemisphere(rec.normal);
     // return 0.5 * (rec.normal + color(1, 1, 1));
-    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+    // return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+    ray scattered;
+    color attenuation;
+    if(rec.mat_ptr->scatter(r, rec, attenuation, scattered)){
+      return attenuation * ray_color(scattered, world, depth - 1);
+    }
+    return color(0, 0, 0);
   }
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -32,8 +39,15 @@ int main(){
 
   // World
   hittable_list world;
-  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+  auto left = make_shared<metal>(color(0.8, 0.8, 0.8));
+  auto right = make_shared<metal>(color(0.8, 0.6, 0.2));
+
+  world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+  world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+  world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, left));
+  world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, right));  
 
   // Camera
   camera cam;
